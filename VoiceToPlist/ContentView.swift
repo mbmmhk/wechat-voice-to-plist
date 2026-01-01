29dbeea4c0b43ee95a746b67723a39d93ccfb401
Sam Gu <sam.gu@crypto.com>
2026-01-01 20:59:31 +0800

    Initial Commit

---
+++ VoiceToPlist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoiceToPlist
+//
+//  Created by Junjie Gu on 2026/1/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoiceToPlist/VoiceToPlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceToPlistApp.swift
+//  VoiceToPlist
+//
+//  Created by Junjie Gu on 2026/1/1.
+//
+
+import SwiftUI
+
+@main
+struct VoiceToPlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
